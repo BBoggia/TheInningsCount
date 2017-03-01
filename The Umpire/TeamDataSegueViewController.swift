799b1954136e1900db7d8ca93baae173bc768812
Branson Boggia <bmanboggia@yahoo.com>
2017-03-01 13:59:17 -0500

    3/1 started on data view

--- The Umpire/TeamDataSegueViewController.swift
+++ The Umpire/TeamDataSegueViewController.swift
@@ -12,15 +12,21 @@ import FirebaseDatabase
 
 class TeamDataSegueViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var ageListTable: UITableView!
+    
     let ref = FIRDatabase.database().reference().child("Database")
+    var ageList: Array<String> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ref.observe(.value, with: { snapshot in
-            
-            var ageGroup
-            
+        ref.observe(.value, with: {
+            snapshot in
+            var ageGroups = [String]()
+            for ageList in snapshot.children {
+                ageGroups.append((ageList as AnyObject).key)
+            }
+            print(ageGroups)
         })
     }
 
@@ -30,25 +36,23 @@ class TeamDataSegueViewController: UIViewController, UITableViewDelegate, UITabl
     }
     
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
         return 1
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return numbers.count
+        return ageList.count
     }
     
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath) 
         
-        // Configure the cell...
-        cell.textLabel?.text = numbers[indexPath.row]
-        cell.backgroundColor = UIColor.clearColor()
+        cell.textLabel?.text = ageList[indexPath.row]
         
         return cell
     }
