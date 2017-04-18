07d865be3b9769872635fb8cc2aad281463170b4
Branson Boggia <bmanboggia@yahoo.com>
2017-04-18 10:33:49 -0400

    4/18 a little work at home

--- The Umpire/AdminTableViewController.swift
+++ The Umpire/AdminTableViewController.swift
@@ -15,11 +15,39 @@ class AdminTableViewController: UITableViewController {
     
     let user = FIRAuth.auth()?.currentUser
     let userUID = FIRAuth.auth()?.currentUser?.uid
+    let ref = FIRDatabase.database().reference()
     
-    var tablePath: String!
+    var decider: Int!
+    
+    var tablePath: FIRDatabaseReference!
+    var convertedArray = [String]()
+    var league: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ref.child("UserData").child(userUID!).child("League").observeSingleEvent(of: .value, with: { (snapshot) in
+            self.league = snapshot.value as! String!
+            self.dataObserver()
+        })
+        
+        switch decider {
+        case 0:
+            tablePath = ref.child(league)
+            print("Rename Age Group")
+            
+        case 1:
+            print("Rename Team")
+            
+        case 2:
+            print("Add/Remove Ages & Teams")
+        
+        case 3:
+            print("Remove a Coach")
+            
+        default:
+            print(LocalizedError.self)
+        }
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
@@ -29,6 +57,19 @@ class AdminTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func dataObserver() {
+        tablePath.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            for child in snapshot.children {
+                let snap = child as! FIRDataSnapshot
+                self.convertedArray.append(snap.value as! String)
+                self.tableView.reloadData()
+            }
+            print(self.convertedArray)
+            self.convertedArray.reverse()
+        })
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -37,13 +78,13 @@ class AdminTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return convertedArray.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = convertedArray[indexPath.row]
 
         return cell
     }
