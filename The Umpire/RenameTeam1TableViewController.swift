477b6593b085781fd8b2d4950d4f9b11dd7247c8
Branson Boggia <bmanboggia@yahoo.com>
2017-04-19 21:37:26 -0400

    4/19 really tired hope i didn't mess anything up no work hardly

--- The Umpire/RenameTeam1TableViewController.swift
+++ The Umpire/RenameTeam1TableViewController.swift
@@ -13,19 +13,22 @@ import FirebaseDatabase
 
 class RenameTeam1TableViewController: UITableViewController {
     
-    let ref = FIRDatabase.database().reference()
+    let ref = FIRAuth.database().currentUser!
+    let user: FIRAuth
+    let userUID = FIRAuth.auth()?.currentUser
     
-    var tablePath: String!
+    var tablePath: FIRDataSnapshot!
     var convertedArray = [String]()
+    var alertTextField: String!
+    var league: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        ref.child("UserData").child(userUID!).child("League").observeSingleEvent(of: .value, with: { (snapshot) in
+            self.league = snapshot.value as! String!
+            self.dataObserver()
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,9 +38,9 @@ class RenameTeam1TableViewController: UITableViewController {
     
     func dataObserver() {
         
-        self.tablePath = self.ref.child("LeagueDatabase").child(self.league)
+        self.tablePath = self.ref.child("LeagueTeamLists").child(self.league)
         
-        tablePath.observeSingleEvent(of: .value, with: { (snapshot) in
+        self.tablePath.observeSingleEvent(of: .value, with: { (snapshot) in
             
             for child in snapshot.children {
                 let snap = child as! FIRDataSnapshot
@@ -52,19 +55,46 @@ class RenameTeam1TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return convertedArray.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = convertedArray[indexPath.row]
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.selectedCell = convertedArray[indexPath.row]
+        
+        let myAlert = UIAlertController(title: "Rename Team", message: "Enter the new name for the team selected.", preferredStyle: UIAlertControllerStyle.alert)
+        
+        myAlert.addTextField()
+        
+        let okAction = UIAlertAction(title: "Confirm", style: UIAlertActionStyle.default) { action in
+            self.alertTextField = myAlert.textFields![0].text
+            
+            self.tablePath.observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                self.ref.child("LeagueDatabase").child(self.league).child(self.alertTextField).setValue(snapshot.childSnapshot(forPath: self.selectedCell).value)
+            })
+            
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        
+        myAlert.addAction(okAction)
+        myAlert.addAction(cancelAction)
+        
+        self.present(myAlert, animated: true, completion: nil)
     }
 }
